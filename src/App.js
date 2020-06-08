1ca8c20767f3dab029afaa50e1e4f32f44cdd8c0
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-06-08 11:08:26 -0500

    eslintrc file added to track compile errors

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Vamos a aprender React
         </p>
         <a
           className="App-link"
@@ -19,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>Esto es un párrafo</p>
     </div>
   );
 }
